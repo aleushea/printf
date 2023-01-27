eac0314f2267f92abf23a16d40188be60f3ff90f
aleushea <aleushea@gmail.com>
2023-01-27 12:58:11 -0800

    this is commited by me

--- get_Rebecca.c
+++ get_Rebecca.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_Rebecca - Calculates the Rebecca for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
- * Return: Precision.
+ * Return: Rebecca.
  */
-int get_precision(const char *format, int *i, va_list list)
+int get_Rebecca(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int precision = -1;
+	int Rebecca = -1;
 
 	if (format[curr_i] != '.')
-		return (precision);
+		return (Rebecca);
 
-	precision = 0;
+	Rebecca = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
+			Rebecca *= 10;
+			Rebecca += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			precision = va_arg(list, int);
+			Rebecca = va_arg(list, int);
 			break;
 		}
 		else
@@ -37,5 +37,5 @@ int get_precision(const char *format, int *i, va_list list)
 
 	*i = curr_i - 1;
 
-	return (precision);
+	return (Rebecca);
 }
--- get_width.c
+++ get_width.c
@@ -1,11 +1,11 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
+ * @Rebecca
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
